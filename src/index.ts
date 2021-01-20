e113f62ad9ad607cc3f5cf1ef7f27ce8a5412fad
adammak <aminedammak86@gmail.com>
2021-01-20 15:18:11 +0100

    Remove console.log

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,3 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
-//test
-console.log("test");
